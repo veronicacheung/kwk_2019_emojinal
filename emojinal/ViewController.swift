2b13b044826308bde5e3ed66a9b84a8f74c7eda1
Apple <apple@Apples-MacBook-Pro-65.local>
2019-07-25 15:10:54 -0400

    df

---
+++ emojinal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  emojinal
+//
+//  Created by Apple on 7/25/19.
+//  Copyright © 2019 Veronica Cheung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
